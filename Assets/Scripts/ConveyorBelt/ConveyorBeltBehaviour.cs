54fe20da6947c2d28460dba46e3832ea94bb767e
auusi9 <ausi_dalmau@hotmail.com>
2020-03-07 13:10:09 +0100

    ConveyorBelt

---
+++ Assets/Scripts/ConveyorBelt/ConveyorBeltBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBeltBehaviour : MonoBehaviour
+{
+    [SerializeField] private Transform _endpoint;
+    [SerializeField] private float _speed;
+
+    private Vector3 _direction;
+
+    private void Start()
+    {
+        _direction = _endpoint.position - transform.position;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Vector3 position = other.transform.position;
+        position = Vector3.MoveTowards(
+                position, 
+                position + _direction, 
+                _speed * Time.deltaTime);
+        other.transform.position = position;
+    }
+}
